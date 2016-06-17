2366bf010540feb7fa19951669c6c88c3d82a322
Stewart Cole <stewart@>
2016-06-17 13:17:43 -0700

    pizza object populates when you press the Make My Pizza button

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,50 @@
+var size;
+var topping1;
+var topping2;
 
+var toppings = [];
 
-function Topping(name, img) {
+function Topping(name, url) {
   this.name = name;
-  this.img = img;
+  this.img = url;
+  toppings.push(this);
+}
+var none = new Topping("none", "none");
+var pineapple = new Topping("pineapple", "img/pineapple.png");
+var mushrooms = new Topping("mushrooms", "img/mushrooms.jpg");
+var spinach = new Topping("spinach", "img/spinach.jpg");
+var tomatoes = new Topping("tomatoes", "img/tomatoes.jpg");
+var artichokes = new Topping("artichokes", "img/artichokes.jpg");
+
+function Pizza(size, topping1, topping2) {
+  this.size;
+  this.topping1;
+  this.topping2;
 }
 
-function Pizza(size) {
-  this.size = size;
-}
 
-Pizza.prototype.topping1(function(topping) {
-  this.topping1 = topping;
-});
+Pizza.prototype.topping1 = function(topping) {
+  return this.topping1 = topping;
+};
 
-Pizza.prototype.topping2(function(topping) {
-  this.topping2 = topping;
-});
+Pizza.prototype.topping2 = function(topping) {
+  return this.topping2 = topping;
+};
+
 
 
 $(document).ready(function() {
-  $("#submit").click(function(event){
+  $("#size").change(function() {
+    size = $("#size").val();
+  });
+  $("#topping1").change(function() {
+    topping1 = parseInt($("#topping1").val())
+  });
+  $("#topping2").change(function() {
+    topping2 = parseInt($("#topping2").val())
+  });
+  $("#getPizza").click(function(event) {
     event.preventDefault();
-
+    var pizza = new Pizza(size, topping1, topping2);
   });
 });
