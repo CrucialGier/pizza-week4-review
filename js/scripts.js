458449805dfc1b7a7d0811dc485cff2ed382048a
Stewart Cole <coleste001@gmail.com>
2016-06-23 16:03:34 -0700

    farter separated front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,38 @@
-var toppings = [];
 
 function Topping(name, url, price) {
   this.name = name;
   this.img = url;
   this.price = price;
-  toppings.push(this);
 }
-var none = new Topping("none", "none", 0);
-var pineapple = new Topping("pineapple", "img/pineapple.png", 2);
-var mushrooms = new Topping("mushrooms", "img/mushrooms.jpg", 1);
-var spinach = new Topping("spinach", "img/spinach.jpg", 1);
-var tomatoes = new Topping("tomatoes", "img/tomatoes.jpg", 1);
-var artichokes = new Topping("artichokes", "img/artichokes.jpg", 3);
+// var none = new Topping("none", "none", 0);
+// var pineapple = new Topping("pineapple", "img/pineapple.png", 2);
+// var mushrooms = new Topping("mushrooms", "img/mushrooms.jpg", 1);
+// var spinach = new Topping("spinach", "img/spinach.jpg", 1);
+// var tomatoes = new Topping("tomatoes", "img/tomatoes.jpg", 1);
+// var artichokes = new Topping("artichokes", "img/artichokes.jpg", 3);
 
 function Pizza(size, topping1, topping2) {
+  this.allToppings = [
+    {"name" : "pineapple", "img" : "img/pineapple.png", "price" : 2},
+    {"name" : "artichokes", "img" : "img/artichokes.jpg", "price" : 3},
+    {"name" : "tomatoes", "img" : "img/tomatoes.jpg", "price" : 1},
+    {"name" : "spinach", "img" : "img/spinach.jpg", "price" : 1},
+    {"name" : "mushrooms", "img" : "img/mushrooms.jpg", "price" : 1},
+    {"name" : "none", "img" : "none", "price" : 0}
+  ];
   this.size = size;
   this.topping1 = topping1;
   this.topping2 = topping2;
 }
 
 Pizza.prototype.createPizza = function() {
-  if (topping1 === 0 && topping2 === 0) {
+  if (this.topping1 === 0 && this.topping2 === 0) {
     return this.result = 0;
-  } else if (topping2 === 0){
+  } else if (this.topping2 === 0){
     return this.result = 1;
-  } else if (topping1 === 0){
+  } else if (this.topping1 === 0){
     return this.result = 2;
-  } else if (topping1 === topping2) {
+  } else if (this.topping1 === this.topping2) {
     return this.result = 3;
   } else {
     return this.result = 4;
@@ -34,17 +40,18 @@ Pizza.prototype.createPizza = function() {
 };
 
 Pizza.prototype.getPrice = function() {
-  return this.price = "Your order comes to a total of $" + (this.topping1.price + this.topping2.price + 10) + ".00. Thank you for dining with us!";
+  return this.price = this.topping1.price + this.topping2.price + 10;
 };
 
 
 $(document).ready(function() {
   var pizza;
   $("#getPizza").click(function(event) {
+    debugger;
     event.preventDefault();
     $("#price").empty();
     $("#confirm").show();
-    pizza = new Pizza($("#size").val(), toppings[parseInt($("#topping1").val())], toppings[parseInt($("#topping2").val())]);
+    pizza = new Pizza($("#size").val(), pizza.allToppings[parseInt($("#topping1").val())], pizza.allToppings[parseInt($("#topping2").val())]);
     pizza.createPizza();
     if (pizza.result === 0) {
       $("#result").text("you want a " + this.size + " cheese pizza?");
@@ -62,6 +69,6 @@ $(document).ready(function() {
     $("#topping2Display").empty().append("<img src='" + pizza.topping2.img + "' alt=''>");
   });
   $("#confirm").click(function() {
-    $("#price").text(pizza.price);
+    $("#price").text("Your order comes to a total of $" + pizza.price + ".00. Thank you for dining with us!");
   });
 });
